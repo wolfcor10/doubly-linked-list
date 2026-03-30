2741ff98c84ef622956057786f810d3f33d3fb68
Wolfang Corredor <corredor.wolfang@gmail.com>
2026-03-30 12:40:12 -0500

    Challenge Done!

---
+++ DoublyLinkedList/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DoublyLinkedList
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var list = new DoublyLinkedList<int>();
+            list.InsertHead(20);
+            list.InsertTail(10);
+            list.InsertTail(30);
+            list.InsertTail(10);
+            list.InsertTail(15);
+            list.InsertTail(20);
+            list.InsertTail(20);
+
+
+            Console.WriteLine("Original List:" + list);
+            Console.WriteLine("Size: " + list.Size);
+            Console.WriteLine("Index of 15: "+ list.Search(15));
+
+
+            list.RemoveByValue(30);
+            Console.WriteLine("After removing 30: " + list);
+            Console.WriteLine("Size: " + list.Size);
+
+            list.RemoveDuplicates();
+            Console.WriteLine("After removing duplicates: "+ list);
+            Console.WriteLine("Size: " + list.Size);
+
+
+        }
+    }
+}
